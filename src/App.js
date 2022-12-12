12d01b45b98be2de8db5ff524051a3e4c8b32f6b
unknown <hamzarq999@gmail.com>
2022-12-12 13:31:23 +0500

    Explored React v8 components and pure React with initializing prettier

---
+++ src/App.js
@@ -0,0 +1,33 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, props.name),
+      React.createElement("h2", {}, props.animal),
+      React.createElement("h2", {}, props.breed),
+    ]);
+  };
+  
+  const App = () => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Learn React"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Bulldog",
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "cat",
+        breed: "broe",
+      }),
+      React.createElement(Pet, {
+        name: "Lemon",
+        animal: "bird",
+        breed: "Cockateil",
+      }),
+    ]);
+  };
+  
+  const container = document.getElementById("root");
+  const root = ReactDOM.createRoot(container);
+  root.render(React.createElement(App));
+  
