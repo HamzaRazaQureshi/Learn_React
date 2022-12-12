8430bfea814aa105cd8a70058d41d842eb16aa7a
unknown <hamzarq999@gmail.com>
2022-12-12 19:39:26 +0500

    adding HTML to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom';
+import Pet from "./Pet";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Learn React"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Bulldog",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "cat",
+//       breed: "broe",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Lemon",
+//       animal: "bird",
+//       breed: "Cockateil",
+//     }),
+//   ]);
+// };
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Learn React"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Bulldog",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "cat",
-      breed: "broe",
-    }),
-    React.createElement(Pet, {
-      name: "Lemon",
-      animal: "bird",
-      breed: "Cockateil",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Learn React V8</h1>
+      <Pet name="Luna" anima="Dog" breed="Bulldog" />
+      <Pet name="Pepper" anima="Cat" breed="broe" />
+      <Pet name="Lemon" anima="bird" breed="Cockateil" />
+    </div>
+  )
 };
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
